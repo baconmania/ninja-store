8b8b893cb6af63ca1a7939f37e5c8e5ceb60b2aa
Soo Philip Kim <philipjkim@gmail.com>
2012-07-09 14:35:44 +0900

    modified auto-generated codes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+var store = require('./routes/store');
 
 var app = module.exports = express.createServer();
 
@@ -32,7 +32,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', store.home);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
