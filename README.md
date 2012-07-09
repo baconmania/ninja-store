4da1f0f5ac0d52d1111855d051efa499f63b2030
Soo Philip Kim <philipjkim@gmail.com>
2012-07-09 15:34:06 +0900

    completed the tutorial

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Express.js Tutorial from hacksparrow.com
 ## References
 
 - http://www.hacksparrow.com/express-js-tutorial.html
+
+- http://expressjs.com/guide.html
+
+- https://github.com/visionmedia/jade#readme
+
+- http://learnboost.github.com/stylus/
--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', store.home);
+app.post('/', store.home_post_handler);
+app.get('/items', store.items);
+app.get('/item/:id', store.item);
+app.get('/page', store.page);
+app.get('/logout', function(req, res) {
+  delete req.session.username;
+  res.redirect('/');
+});
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
